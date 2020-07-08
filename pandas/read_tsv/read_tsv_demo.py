67b6b89170d490d178157ad46fee7e5563bbb670
zlt5610 <zlt56102017@gmai.com>
2020-07-08 15:43:46 +0200

    add tsv_to_excel tool

---
+++ pandas/read_tsv/read_tsv_demo.py
@@ -0,0 +1,19 @@
+import pandas as pd
+#输入文件所在路径
+file_path=input("请输入要转换的文件路径\n")
+#读取tsv文件
+train=pd.read_csv(file_path, sep='\t')
+#获取原文件所在文件夹位置
+list=file_path.split('\\')
+filename=list[-1].split('.')[0]
+
+list=list[:-1]
+path=''
+for l in list:
+    path=path+'\\'+l
+path=path[1:]
+
+path=path+"\\"+filename+'.xlsx'
+#将DataFrame转为Excel
+train.to_excel(path,index=False,header=True)
+print('转换完成')
