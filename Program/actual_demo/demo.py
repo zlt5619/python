66b847895edfbaa50e96dca8561f0369d6da05e4
zlt5610 <779898215@qq.com>
2020-08-10 23:09:28 +0200

    add a autodraw function

--- Program/actual_demo/demo.py
+++ Program/actual_demo/demo.py
@@ -1,7 +1,40 @@
 from tkinter import *
 from tkinter.filedialog import askopenfilenames
 from pyautocad import Autocad, APoint
-import pandas as pd
+import xlrd
+
+acad = Autocad(create_if_not_exists=True)
+
+def draw_switch(position_X,positon_Y,load,switch):
+    pp1=APoint(position_X,positon_Y)
+    pp2=APoint(position_X,positon_Y+200)
+    pp3=APoint(position_X+400,positon_Y+200)
+    pp4=APoint(position_X+400,positon_Y)
+    acad.model.AddLine(pp1, pp2)
+    acad.model.AddLine(pp2, pp3)
+    acad.model.AddLine(pp3, pp4)
+    acad.model.AddLine(pp4, pp1)
+    textString = load
+    insertPnt = APoint(position_X+20, positon_Y+60)
+    height =80
+    acad.model.AddText(textString, insertPnt, height)
+    textString = switch
+    insertPnt = APoint(position_X+220, positon_Y+60)
+    height = 70
+    acad.model.AddText(textString, insertPnt, height)
+
+def read_excel_file(filepath):
+    workxls = xlrd.open_workbook(filepath)
+    worksheet = workxls.sheet_by_name("Sheet1")
+    row = worksheet.nrows  # 总行数
+    file_related_list=[]
+    for i in range(row):
+        if i==0:
+            pass
+        else:
+            rowdata = worksheet.row_values(i)  # i行的list
+            file_related_list.append(rowdata)
+    return file_related_list
 
 def drawcad(mainpowersource,auxlirypowersoruce,filelist):
     global load_list, switch_file
@@ -15,11 +48,31 @@ def drawcad(mainpowersource,auxlirypowersoruce,filelist):
             switch_file=filepath
         else:
             print("出错了")
-    load_list=pd.read_excel(load_list)
-    switch_file=pd.read_excel(switch_file)
-    acad = Autocad(create_if_not_exists=True)
+    load_list=read_excel_file(load_list)
+    switch_file=read_excel_file(switch_file)
+    for row1 in load_list:
+        current = round(row1[1] / row1[2], 2)
+        for row2 in switch_file:
+            if current < row2[1]:
+                row1.append(row2[0])
+                break
 
-
+    p1 = APoint(0, 0)
+    p2 = APoint(0, 2200)
+    p3 = APoint(800, 2200)
+    p4 = APoint(800, 0)
+    acad.model.AddLine(p1, p2)
+    acad.model.AddLine(p2, p3)
+    acad.model.AddLine(p3, p4)
+    acad.model.AddLine(p4, p1)
+    start_X=[0,400]
+    start_Y=1600
+    for i in range(len(load_list)):
+        a=i%2
+        draw_switch(start_X[a],start_Y,load_list[i][0],load_list[i][3])
+        if a==1:
+            start_Y=start_Y-200
+    print("完成绘图")
 filelist=[]
 
 def selectPath(path):
