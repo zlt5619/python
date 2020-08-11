17c867579969df5933dd026d348226490606e142
zlt5610 <779898215@qq.com>
2020-08-11 23:18:46 +0200

    add a autocad  block function

--- Program/actual_demo/demo2/cad.py
+++ Program/actual_demo/demo2/cad.py
@@ -4,40 +4,40 @@ acad = Autocad(create_if_not_exists = True)
 acad.prompt("Hello! AutoCAD from pyautocad.")
 
 class Block():
-    def __init__(self):
-        pass
+    def __init__(self,name,insert_point_x,insert_point_y,*args):
+        self.grip_point=APoint(0,0)
+        self.insert_point=APoint(insert_point_x,insert_point_y)
+        self.name=name
+        self.blockobj=acad.ActiveDocument.Blocks.Add(self.grip_point,name)
+        self.model_list=args
+        for model in self.model_list:
+            if model.name=="矩形":
+                self.blockobj.AddLine(model.p1,model.p2)
+                self.blockobj.AddLine(model.p2,model.p3)
+                self.blockobj.AddLine(model.p3,model.p4)
+                self.blockobj.AddLine(model.p4,model.p1)
+            elif model.name=="文字":
+                self.blockobj.AddText(model.text,model.insert_point,model.height)
+            else:
+                print("出错了")
+        self.RetVal = acad.model.InsertBlock(self.insert_point, name, 1, 1, 1, 0)
 
 class Rectangle():
-    def __init__(self,x,y):
-        self.p1=APoint(0,0)
-        self.p2=APoint(0,y)
-        self.p3=APoint(x,y)
-        self.p4=APoint(x,0)
-        # acad.model.AddLine(self.p1, self.p2)
-        # acad.model.AddLine(self.p2, self.p3)
-        # acad.model.AddLine(self.p3, self.p4)
-        # acad.model.AddLine(self.p4, self.p1)
+    def __init__(self,x,y,insert_x,insert_y):
+        self.p1=APoint(insert_x,insert_y)
+        self.p2=APoint(insert_x,insert_y+y)
+        self.p3=APoint(insert_x+x,insert_y+y)
+        self.p4=APoint(insert_x+x,insert_y)
+        self.name="矩形"
 
-          # 新建块的名称为"HIT_Block"；
-          # grip为块定位夹点所在位置。
+class Text():
+    def __init__(self,text,height,insert_x,insert_y):
+        self.text=text
+        self.height=height
+        self.insert_point=APoint(insert_x,insert_y)
+        self.name="文字"
 
-# center = APoint(40, 10)
-# majAxis = APoint(10, 0, 0)
-# EllObj = blockObj.AddEllipse(center, majAxis, 0.5)
-#
-# insertionPnt = APoint(0, 0)
-# RetVal = acad.model.InsertBlock(insertionPnt, "HIT_Block", 1, 1, 1, 0 )
+# r1=Rectangle(400,200,0,0)
+# t1=Text("hello world",100,20,20)
+# b1=Block("abc",1000,500,r1,t1)
 
-
-
-r=Rectangle(1000,500)
-
-grip = APoint(20, 0)
-blockObj = acad.ActiveDocument.Blocks.Add(grip, "HIT_Block")
-center = APoint(0, 0)
-radius = 1000
-CircleObj = blockObj.AddCircle(center, radius)
-rectangleobj1=blockObj.AddLine(APoint(0,0),APoint(1000,0))
-rectangleobj2=blockObj.AddLine(APoint(0,0),APoint(0,1000))
-insertionPnt = APoint(0, 0)
-RetVal = acad.model.InsertBlock(insertionPnt, "HIT_Block", 1, 1, 1, 0 )
