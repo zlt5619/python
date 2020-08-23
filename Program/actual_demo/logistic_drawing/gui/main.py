dfec13576d5e84bf121187a67fd97cb1300e8daf
zlt5610 <779898215@qq.com>
2020-08-23 21:13:51 +0200

    add a entry function

--- Program/actual_demo/logistic_drawing/gui/main.py
+++ Program/actual_demo/logistic_drawing/gui/main.py
@@ -38,11 +38,17 @@ def create_excel(input_signal,output_signal,data):
         w_output.write(j,0,output_signal[j])
     for k1,v1 in data.items():
         w=ws.add_sheet(sheetname=k1)
-        for k2,v2 in v1.items():
-            i=k2-1
-            for j in range(len(v2)):
-                w.write(i,j,v2[j])
+        if v1==[[]]:
+            pass
+        else:
+            for k2,v2 in v1.items():
+                i=k2-1
+                for j in range(len(v2)):
+                    w.write(i,j,v2[j])
+
     ws.save("输出文件.xls")
+    L1 = Label(shuchu_frame, text="√", width=3)
+    L1.grid(row=0,column=1)
 
 fontStyle1 = tkFont.Font(size=20)
 B3=Button(shuchu_frame,font=fontStyle1,text="输出形成excel表格",command=lambda :create_excel(input_signal,output_signal,data))
@@ -68,9 +74,6 @@ def addBox(frame,button1,button2,button3):
 def subBox(frame,button1,button2,button3):
     row = frame.grid_size()[1]-1
     column =frame.grid_size()[0]
-    print(row)
-    print(column)
-    print(frame.grid_slaves())
     if len(frame.grid_slaves())==8:
         pass
     else:
@@ -79,9 +82,6 @@ def subBox(frame,button1,button2,button3):
         obj2=frame.grid_slaves().pop(0)
         obj2.destroy()
 
-    print("-")
-    print(frame.grid_slaves())
-    print(frame.grid_size())
 #再画一行
 def draw_another_row(f_zong,j):
     row = f_zong.grid_size()[1]
@@ -125,7 +125,7 @@ def collect_info_and_draw_another_row(frame,f_zong,buttom3,bd=None):
         if j==0 or j==1:
             pass
         else:
-            if info_list[j] not in input_signal and info_list[j] not in str1:
+            if info_list[j] not in input_signal and info_list[j] not in str1 and info_list[j] not in output_signal:
                 daichulixinhao.append(info_list[j])
     for k in daichulixinhao:
         if "信号" in k:
@@ -168,23 +168,60 @@ def draw_basic_row(frame,str1,text=None,f_zong=None,d=None):
     B2.grid(row=row, column=6, padx=3)
     B3 = Button(frame, text="完成", command=lambda: collect_info_and_draw_another_row(frame,f_zong,B3,bd=Button_dict))
     B3.grid(row=row, column=7, padx=3)
+#根据输入信号，画新的多行
+def draw_row(frame,str1,i,v,f_zong=None,d=None):
+    frame = frame
+    row = i
+    b_d = d
+    row_list=v
+    column=len(row_list)
+    for j in range(len(row_list)):
+        if j==0 :
+            L1=Label(frame,text=row_list[j], width=8)
+            L1.grid(row=row, column=j)
+            print(0)
+        elif j==1:
+            L2 = Label(frame, text=row_list[j], width=3)
+            L2.grid(row=row, column=j)
+            print(1)
+        elif j%2==0:
+            E2 = Entry(frame,text=row_list[j],width=7)
+            E2.grid(row=row, column=j)
+            print("a")
+        elif j%2==1:
+            E3 = ttk.Combobox(frame, text=row_list[j],value=str1, width=3)
+            E3.grid(row=row, column=3, padx=3)
+            print("b")
+
+    B1 = Button(frame, text="+", command=lambda: addBox(frame, B1, B2, B3))
+    B1.grid(row=row, column=column, padx=3)
+    B2 = Button(frame, text="-", command=lambda: subBox(frame, B1, B2, B3))
+    B2.grid(row=row, column=column+1, padx=3)
+    B3 = Button(frame, text="完成", command=lambda: collect_info_and_draw_another_row(frame, f_zong, B3, bd=Button_dict))
+    B3.grid(row=row, column=column+2, padx=3)
+
+
 #汇总整个信号的所有输入
-def get_all_info(output,bd=None):
+def get_all_info(output,bd=None,root=None):
     #在传输数据时，需要将数据取出到函数中，再处理，否则受原对象影响
     Button_dict=bd
     k_list=[]
     v_list=[]
     add_dict={}
+    root1=root
     for k,v in Button_dict.items():
         k_list.append(k)
         v_list.append(v)
     for i in range(len(k_list)):
         add_dict[k_list[i]]=v_list[i]
     data[output]= add_dict
+    print(data)
     Button_dict.clear()
+    L1 = Label(root1, text="√", width=3)
+    L1.pack()
 
 #跳转到信号逻辑输入界面
-def jump_to_shuchu(output,data):
+def jump_to_shuchu(output,value):
 
     root1=Tk(className=output+"的逻辑表达式输入")
     root1.geometry("600x600")
@@ -195,17 +232,18 @@ def jump_to_shuchu(output,data):
     f1 = Frame(f_zong, height=20, width=600)
     f1.grid(row=0, sticky=W)
     f1.content_dict=dict()
-    b = Button(root1, text="完成编写", command=lambda: get_all_info(output,bd=Button_dict))
+    b = Button(root1, text="完成编写", command=lambda: get_all_info(output,bd=Button_dict,root=root1))
 
     f_zong.pack()
     b.pack()
-    if data==0:
-        #当首次没有输入的时候
-        draw_basic_row(f1,str1,text=output,f_zong=f_zong,d=Button_dict)
+    if value!=[]:
+        #当data有输入的时候
+        for i in range(len(value)):
+            draw_row(f1,str1,i,value[i],f_zong=f_zong,d=Button_dict)
 
     else:
-        #当有参数传递进来的时候
-        pass
+        #当没有参数传递进来的时候
+        draw_basic_row(f1,str1,text=output,f_zong=f_zong,d=Button_dict)
 
     root1.mainloop()
 
@@ -215,18 +253,19 @@ def jump_to_shuchu(output,data):
 def draw_button_frame(output_signal,data):
     output_signal=output_signal
     buttons=[]
+    #输入data为{'PO002': [['PO002', '=', 'MT001', '&&', 'MT002']],
+    # 'PO005': [['PO005', '=', '信号1', '&&', '信号2', '||', 'MT003'], ['信号1', '=', 'MT004', '||', 'MT005', '', ''], ['信号2', '=', 'Mt006', '&&', 'MT007', '', '']],
+    #  'PO001': [[]], 'PO003': [[]], 'PO004': [[]], 'PO006': [[]], 'PO007': [[]], 'PO008': [[]], 'PO009': [[]], 'PO010': [[]], 'PO011': [[]], 'PO012': [[]], 'PO013': [[]], 'PO014': [[]], 'PO015': [[]], 'PO016': [[]], 'PO017': [[]], 'PO018': [[]], 'PO019': [[]], 'PO020': [[]]}}
+
     for index in range(len(output_signal)):
         row=int(index/6)
         column=index%6
         name=output_signal[index]
-        if data:
-            pass
-        else:
-            data=0
-            button = Button(button_frame, text=name,
-                            command=lambda name=name: jump_to_shuchu(name,data))
-            button.grid(padx=2, pady=2, row=row, column=column)
-            buttons.append(button)
+        value=data[name]
+        button = Button(button_frame, text=name,
+                            command=lambda name=name,value=value: jump_to_shuchu(name,value))
+        button.grid(padx=2, pady=2, row=row, column=column)
+        buttons.append(button)
 
 
 """
@@ -274,6 +313,10 @@ def read_file(filelist,data1):
                 rowdata = worksheet.row_values(j)
                 data_list.append(rowdata)
             data[i]=data_list
+    for shuchu in output_signal:
+        if shuchu not in data.keys():
+            data[shuchu]=[[]]
+
     print("字典为",end=None)
     print(data)
 
