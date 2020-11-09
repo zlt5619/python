75a0b0286fb175d251e040c0369e18ce6b7acdec
zlt5610 <779898215@qq.com>
2020-11-09 15:23:18 +0800

    add a cad function

--- CAD/block/load_model/demo.py
+++ CAD/block/load_model/demo.py
@@ -1,14 +1,20 @@
 from pyautocad import APoint
 
 class load_model():
-    def __init__(self,start_point=(0,0),chang=1000,kuan=500,load_name=None):
-        self.point1=APoint(start_point[0],start_point[1])
-        self.point2=APoint(start_point[0]+chang,start_point[1])
-        self.point3=APoint(start_point[0]+chang,start_point[1]+kuan)
-        self.point4=APoint(start_point[0],start_point[1]+kuan)
-        self.wenzi_insert_point=APoint(start_point[0]+chang/2,start_point[1]+kuan/2)
+    #定义每一个负荷的标准
+    def __init__(self,load_name=None):
+        chang=1000
+        kuan=500
+        wenzi_height=300
+        AlignNum = 4
+        self.point1=APoint(0,0)
+        self.point2=APoint(chang,0)
+        self.point3=APoint(chang,kuan)
+        self.point4=APoint(0,kuan)
+        self.wenzi_insert_point=APoint(chang/2,kuan/2)
         self.load_name=load_name
-        self.wenzi_height=300
+        self.wenzi_height=wenzi_height
+        self.wenzi_alignNum=AlignNum
 
     @property
     def draw_info(self):
@@ -17,8 +23,9 @@ class load_model():
         line3=[self.point3,self.point4]
         line4=[self.point4,self.point1]
         line_info=[line1,line2,line3,line4]
-        wenzi_info=[self.load_name,self.wenzi_insert_point,self.wenzi_height]
-        info=[line_info,wenzi_info]
+        wenzi_info=[self.load_name,self.wenzi_insert_point,self.wenzi_height,self.wenzi_alignNum]
+        arc_info=[]
+        info=[line_info,wenzi_info,arc_info]
         return info
 
 
